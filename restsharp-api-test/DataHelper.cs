97ae1676574e37e31bca7e836faaf8e8bd155c71
Mahesh Ganegoda <mganegoda@wiley.com>
2023-10-09 13:13:44 +0530

    refactor

---
+++ restsharp-api-test/DataHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using restsharp_api_test.model;
+
+namespace restsharp_api_test
+{
+    internal class DataHelper
+    {
+        public static Phone getPhoneData() {
+            Phone phone = new Phone();
+            phone.Name = "Apple 26";
+            Data data = new Data();
+            data.Year = 2023;
+            data.Price = 100;
+            data.CpuModel = "Intel Core 9";
+            data.HardDiskSize = "1 TB";
+            phone.Data = data;
+            return phone; }
+    }
+}
--- restsharp-api-test/PhoneTest.cs
+++ restsharp-api-test/PhoneTest.cs
@@ -7,74 +7,59 @@ namespace restsharp_api_test
     {
         [Fact]
          public void testListOfAllObjects()
-            {
-                APIHelper objectsApiHelper = new APIHelper();
-                ICollection<Phone> phoneList = objectsApiHelper.getPhoneList();
-                Assert.NotNull(phoneList);
-            }
+        {
+            APIHelper objectsApiHelper = new APIHelper();
+            ICollection<Phone> phoneList = objectsApiHelper.getPhoneList();
+            Assert.NotNull(phoneList);
+        }
 
         [Fact]
         public void testSingleObjects()
-            {
-                APIHelper objectsApiHelper = new APIHelper();
-                Phone phone = objectsApiHelper.getPhoneById("7");
-                Assert.Equal("7",phone.Id);
-            }
+        {
+            Phone phone = DataHelper.getPhoneData();
+            APIHelper objectsApiHelper = new APIHelper();
+            Phone createdPhone = objectsApiHelper.createPhone(phone);
+            Phone phonecreated = objectsApiHelper.getPhoneById(createdPhone.Id);
+            Assert.Equal(createdPhone.Id, phonecreated.Id);
+        }
 
         [Fact]
         public void testCreateObjects()
-            {
-                Phone phone = new Phone();
-                phone.Name = "Apple 26";
-                Data data = new Data();
-                data.Year = 2023;
-                data.Price = 100;
-                data.CpuModel = "Intel Core 9";
-                data.HardDiskSize = "1 TB";
-                phone.Data = data;
-                APIHelper objectsApiHelper = new APIHelper();
-                Phone createdPhone = objectsApiHelper.createPhone(phone);
-                Assert.Equal(phone.Name, createdPhone.Name);
+        {
+            Phone phone = DataHelper.getPhoneData();
+            APIHelper objectsApiHelper = new APIHelper();
+            Phone createdPhone = objectsApiHelper.createPhone(phone);
+            Assert.Equal(phone.Name, createdPhone.Name);
+            Assert.Equal(phone.Data.Year, createdPhone.Data.Year);
+            Assert.Equal(phone.Data.Price, createdPhone.Data.Price);
+            Assert.Equal(phone.Data.CpuModel, createdPhone.Data.CpuModel);
+            Assert.Equal(phone.Data.HardDiskSize, createdPhone.Data.HardDiskSize);
         }
 
         [Fact]
         public void testUpdateObject()
-            {
-                Phone phone = new Phone();
-                phone.Name = "Apple 26";
-                Data data = new Data();
-                data.Year = 2023;
-                data.Price = 100;
-                data.CpuModel = "Intel Core 9";
-                data.HardDiskSize = "1 TB";
-                phone.Data = data;
-                APIHelper objectsApiHelper = new APIHelper();
-                Phone createdPhone = objectsApiHelper.createPhone(phone);
-                var newName = "Apple New";
-                var newPrice = 200;
-                createdPhone.Name = newName;
-                createdPhone.Data.Price = newPrice;
-                Phone updatedPhone = objectsApiHelper.updatePhone(createdPhone);
-                Assert.Equal(newName, updatedPhone.Name);
-                Assert.Equal(newPrice, updatedPhone.Data.Price);
-            }
+        {
+            Phone phone = DataHelper.getPhoneData();
+            APIHelper objectsApiHelper = new APIHelper();
+            Phone createdPhone = objectsApiHelper.createPhone(phone);
+            var newName = "Apple New";
+            var newPrice = 200;
+            createdPhone.Name = newName;
+            createdPhone.Data.Price = newPrice;
+            Phone updatedPhone = objectsApiHelper.updatePhone(createdPhone);
+            Assert.Equal(newName, updatedPhone.Name);
+            Assert.Equal(newPrice, updatedPhone.Data.Price);
+        }
 
         [Fact]
         public void testDeleteObject()
-            {
-                Phone phone = new Phone();
-                phone.Name = "Apple 26";
-                Data data = new Data();
-                data.Year = 2023;
-                data.Price = 100;
-                data.CpuModel = "Intel Core 9";
-                data.HardDiskSize = "1 TB";
-                phone.Data = data;
-                APIHelper objectsApiHelper = new APIHelper();
-                Phone createdPhone = objectsApiHelper.createPhone(phone);
-                DeletedMsg deletedMsg = objectsApiHelper.deletePhone(createdPhone.Id);
-                var expectedMsg = "Object with id = " + createdPhone.Id + " has been deleted.";
-                Assert.Equal(expectedMsg, deletedMsg.Message);
-            }
+        {
+            Phone phone = DataHelper.getPhoneData();
+            APIHelper objectsApiHelper = new APIHelper();
+            Phone createdPhone = objectsApiHelper.createPhone(phone);
+            DeletedMsg deletedMsg = objectsApiHelper.deletePhone(createdPhone.Id);
+            var expectedMsg = "Object with id = " + createdPhone.Id + " has been deleted.";
+            Assert.Equal(expectedMsg, deletedMsg.Message);
+        }
     }
 } 
